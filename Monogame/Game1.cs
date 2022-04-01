b80a4faa6a9f928c483f95fe4ebb6c6185a39af2
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-04-01 15:45:33 +0200

    Update Game1.cs

--- Monogame/Game1.cs
+++ Monogame/Game1.cs
@@ -10,11 +10,15 @@ namespace Monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D skepp;
-        Texture2D rect;
+        Rectangle rect1;
+        Rectangle rect2;
 
-        Rectangle recta = new Rectangle(50,50,500,500);
+        int rect1startX = 250;
+        int rect2startX = 500;
+        int rectStart = 445;
+        
 
-        Vector2 rectPos = new Vector2(100, 100);
+
 
         public Game1()
         {
@@ -23,24 +27,64 @@ namespace Monogame
             IsMouseVisible = true;
             
         }
-
         protected override void Initialize()
         {
             base.Initialize();
 
         }
-
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            rect = new Texture2D(GraphicsDevice, 20, 20);
             skepp = Content.Load<Texture2D>("skepp");
+            rect1 = new Rectangle(rect1startX,rectStart,20,30);
+            rect2 = new Rectangle(rect2startX,rectStart,20,30);
         }
-
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                rect1.Y --;
+            }
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                rect1.Y ++;
+            }
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                rect2.Y --;
+            }
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                rect2.Y ++;
+            }
+
+            if (rect1.Y < 0)
+            {
+                rect1.Y = rectStart;
+            }
+            else if (rect1.Y > rectStart)
+            {
+                rect1.Y = rectStart;
+            }
+
+            if (rect2.Y < 0)
+            {
+                rect2.Y = rectStart;
+            }
+            else if (rect2.Y > rectStart)
+            {
+                rect2.Y = rectStart;
+            }
+
+            
 
             base.Update(gameTime);
         }
@@ -49,7 +93,8 @@ namespace Monogame
         {
             GraphicsDevice.Clear(Color.Black);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(skepp,rectPos, recta, Color.White);
+            _spriteBatch.Draw(skepp, rect1, Color.Cyan);
+            _spriteBatch.Draw(skepp, rect2, Color.Red);
             _spriteBatch.End();
 
             base.Draw(gameTime);
