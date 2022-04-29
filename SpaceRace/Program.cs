527af0303760f4fde0778fc8f7d85b3146a78035
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-04-29 16:01:57 +0200

    Add files via upload

---
+++ SpaceRace/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceRace2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (SpaceRace.Game1 game = new SpaceRace.Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
